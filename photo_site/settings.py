3dd472331b2adbd3dd9e27ab01aa44542dee415e
TrevorMumm <teerevv@gmail.com>
2024-05-28 16:14:23 -0500

    Prepare Django project for Heroku deployment

--- photo_site/settings.py
+++ photo_site/settings.py
@@ -5,6 +5,11 @@ import boto3
 from botocore.client import Config
 import logging
 logging.basicConfig(level=logging.DEBUG)
+import django_heroku
+from decouple import config
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -177,7 +182,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import django_heroku
-django_heroku.settings(locals())
 # settings.py
